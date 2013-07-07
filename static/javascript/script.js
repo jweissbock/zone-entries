f974af34f56bbb19e45bb98a143ab3514870b0b3
Josh Weissbock <josh.weissbock@gmail.com>
2013-07-07 11:48:08 -0400

    adding javascript to add zentries table to add and delete rows

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,3 +1,32 @@
 $(document).ready(function() {
+	// add new row to the zone entry table
+	$(document).on("click", "#NewRow", function(e) {
+		// clone bottom row
+		var newRow = $("#zentries tr:last").html();
+		newRow = '<tr>'+newRow+'</tr>';
+		// remove all buttons
+		$("#NewRow").remove();
+		// append clone
+		$("#zentries").append(newRow);
+	});
 
+	// remove a table row
+	$(document).on("click", ".DeleteCell", function(e) {
+		// clone last row just in case
+		var newRow = $("#zentries tr:last").html();
+		newRow = '<tr>'+newRow+'</tr>';
+		// delete this row
+		newDelete = $("#NewRow").parent().html();
+		$(this).parent().remove();
+		// adds 'new button' to last row
+		$("#zentries tr:last .NewCell").html(newDelete);
+		// count rows, if zero, add
+		if ($("#zentries tr").length <= 1) {
+			$("#zentries").append(newRow);
+		}
+	});
+
+	$("#SaveZentries").on('click', function() {
+		$.jGrowl('Does Nothing yet');
+	});
 });
